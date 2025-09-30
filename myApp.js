4a00f32c55e2c7fa2002596e46b6b26311bf8910
YELTSIN <yeltsing57@gmail.com>
2025-09-30 00:23:29 -0300

    JSON en una ruta específica

--- myApp.js
+++ myApp.js
@@ -1,16 +1,17 @@
-let express = require('express');
-let path = require('path');
+let express = require("express");
+let path = require("path");
 let app = express();
 
 console.log("Hola Mundo");
 
+app.use("/public", express.static(path.join(__dirname, "public")));
 
-app.use('/public', express.static(__dirname + '/public'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
 
-
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+app.get("/json", (req, res) => {
+  res.json({ message: "Hello json" });
 });
 
 module.exports = app;
