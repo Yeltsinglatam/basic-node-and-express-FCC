b4206fc7864aff5fd04e8a28e853f4834b6da886
YELTSIN <yeltsing57@gmail.com>
2025-09-30 00:17:59 -0300

    Servir activos estáticos

--- myApp.js
+++ myApp.js
@@ -1,11 +1,16 @@
-let express = require("express");
-let path = require("path");
+let express = require('express');
+let path = require('path');
 let app = express();
 
 console.log("Hola Mundo");
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+
+app.use('/public', express.static(__dirname + '/public'));
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 module.exports = app;
