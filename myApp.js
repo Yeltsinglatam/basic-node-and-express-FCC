f3edeb29d6c3fcbcc0f2ad306b837caa010326d1
YELTSIN <yeltsing57@gmail.com>
2025-09-30 00:10:25 -0300

    servidor expreso

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 let express = require("express");
 let app = express();
 
-console.log("Hello World");
+console.log("Hola Mundo");
+
+app.get("/", function (req, res) {
+  res.send("Hello Express");
+});
 
 module.exports = app;
